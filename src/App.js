7f5e053bd29ac7c548bc90d923a2aa6b3fabcee9
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-22 19:32:45 -0500

    counter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import HiMom from './components/HiMom'
+import Counter from './components/Counter'
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
                 someBitOfData={[1, 2, 3, 4, 5]}
                 name="Mama"
             />
+            <Counter />
         </>
     )
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <h1>Counter Component</h1>
+            <p>{count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+    )
+}
